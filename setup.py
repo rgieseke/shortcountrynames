640b169a3595c5aace73787f47ee68c45ca5f74d
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2018-04-17 16:39:05 +0200

    Add Readme as long description

--- setup.py
+++ setup.py
@@ -10,17 +10,25 @@ See README.md and repository for details:
     https://github.com/rgieseke/shortcountrynames
 """
 
+import os
 from setuptools import setup
 
 import versioneer
 
+path = os.path.abspath(os.path.dirname(__file__))
+
 cmdclass = versioneer.get_cmdclass()
 
+with open(os.path.join(path, 'README.md'), "r") as f:
+    readme = f.read()
+    readme = readme.split("\n\n", 1)[1]
 
 setup(
     name='shortcountrynames',
     version=versioneer.get_version(),
     description='Maps from two or three letter country codes to short names',
+    long_description=readme,
+    long_description_content_type='text/markdown',
     url='https://github.com/rgieseke/shortcountrynames',
     author='Robert Gieseke',
     author_email='robert.gieseke@pik-potsdam.de',
